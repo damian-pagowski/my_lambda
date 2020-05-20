dea31ad89fd975f2338784d6e24da494bf8b439a
Damian Pagowski <d.pagowski@gmail.com>
2020-05-20 22:11:44 +0700

    ready for deployment

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const fetchUsers = async () =>
-  await (await fetch("http://localhost:9000/getUsers")).json();
+  await (await fetch("/.netlify/functions/getUsers")).json();
 
 fetchUsers().then((data) => {
   const userList = document.getElementById("users");
