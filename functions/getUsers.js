00031d83c20d1f7ac6e2da404d8fb51a9cabf237
Damian Pagowski <d.pagowski@gmail.com>
2020-05-20 22:07:59 +0700

    ready for deployment

---
+++ functions/getUsers.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+
+exports.handler = function (event, context, callback) {
+  const API_URL = "https://api.github.com/users";
+  const API_CLIENT_SECRET = process.env.API_CLIENT_SECRET;
+  const API_CLIENT_ID = process.env.API_CLIENT_ID;
+
+  const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`;
+
+  // send user response
+  const send = (body) => {
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "origin, X-Requested-With, Content-Type, Accept",
+      },
+      body: JSON.stringify(body),
+    });
+  };
+
+  // perform api call to GH
+
+  const getUsers = () => {
+    axios
+      .get(URL)
+      .then((res) => send(res.data))
+      .catch((err) => send(err));
+  };
+
+  // make sure method is get
+
+  if (event.httpMethod == "GET") {
+    getUsers();
+  }
+};
